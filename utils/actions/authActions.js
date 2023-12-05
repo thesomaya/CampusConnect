5d224010f1d443ab569d4bb18b39267f5742679d
Somaia <somaia.h.shaaban@gmail.com>
2023-12-05 13:25:33 +0300

    notifications

--- utils/actions/authActions.js
+++ utils/actions/authActions.js
@@ -1,4 +1,6 @@
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import * as Device from  "expo-device";
+import * as Notifications from 'expo-notifications';
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirebaseApp } from "../firebaseHelper";
 import {
@@ -45,9 +47,10 @@ export const signUp = (firstName, lastName, studentNumber, email, password) => {
 
       dispatch(authenticate({ token: accessToken, userData }));
       saveDataToStorage(accessToken, uid, expiryDate);
+      await storePushToken(userData);
 
       timer = setTimeout(() => {
-        dispatch(userLogout());
+        dispatch(userLogout(userData));
       }, millisecondsUntilExpiry);
     } catch (error) {
       console.log(error);
@@ -83,9 +86,10 @@ export const signIn = (email, password) => {
 
       dispatch(authenticate({ token: accessToken, userData }));
       saveDataToStorage(accessToken, uid, expiryDate);
+      await storePushToken(userData);
 
       timer = setTimeout(() => {
-        dispatch(userLogout());
+        dispatch(userLogout(userData));
       }, millisecondsUntilExpiry);
     } catch (error) {
       const errorCode = error.code;
@@ -104,8 +108,14 @@ export const signIn = (email, password) => {
   };
 };
 
-export const userLogout = () => {
+export const userLogout = (userData) => {
   return async (dispatch) => {
+    try {
+      await removePushToken(userData);
+    } catch (error) {
+      console.log(error);
+    }
+    
     AsyncStorage.clear();
     clearTimeout(timer);
     dispatch(logout());
@@ -157,3 +167,70 @@ const saveDataToStorage = (token, userId, expiryDate) => {
     })
   );
 };
+
+const storePushToken = async (userData) => {
+  if(!Device.isDevice) {
+    return; 
+  }
+  const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+  const tokenData = { ...userData.pushTokens } || {};
+  const tokenArray = Object.values(tokenData);
+
+  if (tokenArray.includes(token)) return;
+
+  tokenArray.push(token);
+
+ 
+  for (let i = 0; i < tokenArray.length; i++) {
+    const tok = tokenArray[i];
+    tokenArray[i] = tok;
+  }
+
+  const app = getFirebaseApp();
+  const dbRef = ref(getDatabase(app));
+  const userRef = child(dbRef, `users/${userData.userId}/pushTokens`);
+
+  await set(userRef, tokenData);
+}
+
+const removePushToken = async (userData) => {
+  if(!Device.isDevice) {
+    return; 
+  }
+  const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+  const tokenData = await getUserPushTokens(userData.userId);
+ 
+  for (const key in tokenData) {
+    if (tokenData[key] === token) {
+      delete tokenData[key];
+      break;
+    }
+  }
+
+  const app = getFirebaseApp();
+  const dbRef = ref(getDatabase(app));
+  const userRef = child(dbRef, `users/${userData.userId}/pushTokens`);
+
+  await set(userRef, tokenData);
+}
+
+export const getUserPushTokens = async (userId) => {
+  try {
+  const app = getFirebaseApp();
+  const dbRef = ref(getDatabase(app));
+  const userRef = child(dbRef, `users/${userId}/pushTokens`);
+
+  const snapshot = await get(userRef);
+  if(!snapshot || !snapshot.exists()) {
+    return {};
+  }
+
+  return snapshot.val() || {};
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
